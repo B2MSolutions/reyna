c375573ccbfbede93ee9a85d50ce544bac75f23b
jamesbloomer <github2@thebloomers.co.uk>
2013-06-07 13:00:19 +0100

    Fixes #2: If the body contains Chinese characters they are converted to ????

--- reyna-test/src/com/b2msolutions/reyna/DispatcherTest.java
+++ reyna-test/src/com/b2msolutions/reyna/DispatcherTest.java
@@ -1,9 +1,17 @@
 package com.b2msolutions.reyna;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import com.b2msolutions.reyna.Dispatcher.Result;
+import com.b2msolutions.reyna.http.HttpPost;
+import com.b2msolutions.reyna.http.IgnoreCertsHttpClient;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 
 import java.io.IOException;
 import java.net.URI;
@@ -14,17 +22,8 @@ import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-
-import com.b2msolutions.reyna.Dispatcher.Result;
-import com.b2msolutions.reyna.http.HttpPost;
-import com.b2msolutions.reyna.http.IgnoreCertsHttpClient;
-import com.xtremelabs.robolectric.RobolectricTestRunner;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 @RunWith(RobolectricTestRunner.class)
 public class DispatcherTest {
@@ -45,9 +44,39 @@ public class DispatcherTest {
 		assertEquals(Result.OK, new Dispatcher().sendMessage(message, httpPost, httpClient));
 		
 		this.verifyHttpPost(message, httpPost);
+
 		verify(httpClient).setPort(443);
-		verify(httpPost).setEntity("body");
+        ArgumentCaptor<StringEntity> stringEntityCaptor = ArgumentCaptor.forClass(StringEntity.class);
+        verify(httpPost).setEntity(stringEntityCaptor.capture());
+        StringEntity stringEntity = stringEntityCaptor.getValue();
+        assertEquals(stringEntity.getContentType().getValue(), "text/plain; charset=UTF-8");
+        assertEquals(EntityUtils.toString(stringEntity), "body");
 	}
+
+    @Test
+    public void sendMessageHappyPathWithChineseCharactersShouldSetExecuteCorrectHttpPostAndReturnOK() throws URISyntaxException, ClientProtocolException, IOException, KeyManagementException, UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException, CertificateException {
+        Message message = RepositoryTest.getMessageWithHeaders("谷歌拼音输入法");
+
+        StatusLine statusLine = mock(StatusLine.class);
+        when(statusLine.getStatusCode()).thenReturn(200);
+        HttpResponse httpResponse = mock(HttpResponse.class);
+        when(httpResponse.getStatusLine()).thenReturn(statusLine);
+
+        HttpPost httpPost = mock(HttpPost.class);
+        IgnoreCertsHttpClient httpClient = mock(IgnoreCertsHttpClient.class);
+        when(httpClient.execute(httpPost)).thenReturn(httpResponse);
+
+        assertEquals(Result.OK, new Dispatcher().sendMessage(message, httpPost, httpClient));
+
+        this.verifyHttpPost(message, httpPost);
+
+        verify(httpClient).setPort(443);
+        ArgumentCaptor<StringEntity> stringEntityCaptor = ArgumentCaptor.forClass(StringEntity.class);
+        verify(httpPost).setEntity(stringEntityCaptor.capture());
+        StringEntity stringEntity = stringEntityCaptor.getValue();
+        assertEquals(stringEntity.getContentType().getValue(), "text/plain; charset=UTF-8");
+        assertEquals(EntityUtils.toString(stringEntity), "谷歌拼音输入法");
+    }
 	
 	@Test
 	public void sendMessageHappyPathWithPortShouldSetPort() throws URISyntaxException, ClientProtocolException, IOException, KeyManagementException, UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException, CertificateException {
@@ -65,7 +94,11 @@ public class DispatcherTest {
 		assertEquals(Result.OK, new Dispatcher().sendMessage(message, httpPost, httpClient));
 		
 		verify(httpClient).setPort(9008);
-		verify(httpPost).setEntity("body");
+        ArgumentCaptor<StringEntity> stringEntityCaptor = ArgumentCaptor.forClass(StringEntity.class);
+        verify(httpPost).setEntity(stringEntityCaptor.capture());
+        StringEntity stringEntity = stringEntityCaptor.getValue();
+        assertEquals(stringEntity.getContentType().getValue(), "text/plain; charset=UTF-8");
+        assertEquals(EntityUtils.toString(stringEntity), "body");
 	}
 
 	@Test
