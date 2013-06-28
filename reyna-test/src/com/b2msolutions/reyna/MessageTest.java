a8e27c87a37b36bac1a8dbc3fb1291ef9cc6eaa6
jamesbloomer <github2@thebloomers.co.uk>
2013-06-28 12:12:03 +0100

    Now allow connections over http in addition to https

--- reyna-test/src/com/b2msolutions/reyna/MessageTest.java
+++ reyna-test/src/com/b2msolutions/reyna/MessageTest.java
@@ -1,22 +1,21 @@
 package com.b2msolutions.reyna;
 
-import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
 
 public class MessageTest {
 
 	@Test
-	public void whenConstructingWithHTTPSShouldNotThrow()
-			throws URISyntaxException {
+	public void whenConstructingWithHTTPSShouldNotThrow() throws URISyntaxException {
 		assertNotNull(new Message(new URI("https://google.com"), "", null));
 	}
 
-	@Test(expected = UnsupportedOperationException.class)
-	public void whenConstructingWithHTTPShouldThrow() throws URISyntaxException {
-		new Message(new URI("http://google.com"), "", null);
+	@Test
+	public void whenConstructingWithHTTPShouldNotThrow() throws URISyntaxException {
+        assertNotNull(new Message(new URI("http://google.com"), "", null));
 	}
 }
