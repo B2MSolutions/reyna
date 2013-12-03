7839085dd836e6f2d6fff9e9c025467ce1117303
youhana-hana <meyouhana@gmail.com>
2013-12-03 10:48:33 +0000

    [#54359426] Gzip content if gzip content-encoding specified

--- reyna-test/src/com/b2msolutions/reyna/DispatcherTest.java
+++ reyna-test/src/com/b2msolutions/reyna/DispatcherTest.java
@@ -1,19 +1,29 @@
 package com.b2msolutions.reyna;
 
+import android.content.Context;
+import android.net.http.AndroidHttpClient;
 import com.b2msolutions.reyna.Dispatcher.Result;
 import com.b2msolutions.reyna.http.HttpPost;
+import com.b2msolutions.reyna.shadows.ShadowAndroidHttpClient;
+import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.entity.AbstractHttpEntity;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.message.BufferedHeader;
 import org.apache.http.util.EntityUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.KeyManagementException;
@@ -21,12 +31,25 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import java.util.zip.GZIPOutputStream;
 
+import static android.test.MoreAsserts.assertNotEqual;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.*;
 
 @RunWith(RobolectricTestRunner.class)
 public class DispatcherTest {
+
+    private Context context;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.context = Robolectric.getShadowApplication().getApplicationContext();
+        Robolectric.bindShadowClass(ShadowAndroidHttpClient.class);
+    }
 
 	@Test
 	public void sendMessageHappyPathShouldSetExecuteCorrectHttpPostAndReturnOK() throws URISyntaxException, ClientProtocolException, IOException, KeyManagementException, UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException, CertificateException {
@@ -40,8 +63,8 @@ public class DispatcherTest {
 		HttpPost httpPost = mock(HttpPost.class);
 		HttpClient httpClient = mock(HttpClient.class);
 		when(httpClient.execute(httpPost)).thenReturn(httpResponse);
-		
-		assertEquals(Result.OK, new Dispatcher().sendMessage(message, httpPost, httpClient));
+
+		assertEquals(Result.OK, new Dispatcher().sendMessage(message, httpPost, httpClient, this.context));
 		
 		this.verifyHttpPost(message, httpPost);
 
@@ -65,7 +88,7 @@ public class DispatcherTest {
         HttpClient httpClient = mock(HttpClient.class);
         when(httpClient.execute(httpPost)).thenReturn(httpResponse);
 
-        assertEquals(Result.OK, new Dispatcher().sendMessage(message, httpPost, httpClient));
+        assertEquals(Result.OK, new Dispatcher().sendMessage(message, httpPost, httpClient, this.context));
 
         this.verifyHttpPost(message, httpPost);
 
@@ -89,7 +112,7 @@ public class DispatcherTest {
         HttpClient httpClient = mock(HttpClient.class);
         when(httpClient.execute(httpPost)).thenReturn(httpResponse);
 		
-		assertEquals(Result.OK, new Dispatcher().sendMessage(message, httpPost, httpClient));
+		assertEquals(Result.OK, new Dispatcher().sendMessage(message, httpPost, httpClient, this.context));
 		
         ArgumentCaptor<StringEntity> stringEntityCaptor = ArgumentCaptor.forClass(StringEntity.class);
         verify(httpPost).setEntity(stringEntityCaptor.capture());
@@ -106,7 +129,7 @@ public class DispatcherTest {
         HttpClient httpClient = mock(HttpClient.class);
 		when(httpClient.execute(httpPost)).thenThrow(new RuntimeException(""));
 		
-		assertEquals(Result.TEMPORARY_ERROR, new Dispatcher().sendMessage(message, httpPost, httpClient));
+		assertEquals(Result.TEMPORARY_ERROR, new Dispatcher().sendMessage(message, httpPost, httpClient, this.context));
 		
 		this.verifyHttpPost(message, httpPost);
 	}
@@ -121,12 +144,75 @@ public class DispatcherTest {
 		assertEquals(Result.PERMANENT_ERROR, Dispatcher.getResult(600));
 	}
 
+    @Test
+    public void sendMessageWithGzipAndContentIsLessThanMinGzipLengthShouldRemoveGzipHeaderAndSendMessageAsString() throws URISyntaxException, ClientProtocolException, IOException, KeyManagementException, UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException, CertificateException, Exception {
+        Message message = RepositoryTest.getMessageWithGzipHeaders("body");
+
+        StatusLine statusLine = mock(StatusLine.class);
+        when(statusLine.getStatusCode()).thenReturn(200);
+        HttpResponse httpResponse = mock(HttpResponse.class);
+        when(httpResponse.getStatusLine()).thenReturn(statusLine);
+
+        HttpPost httpPost = mock(HttpPost.class);
+        HttpClient httpClient = mock(HttpClient.class);
+        when(httpClient.execute(httpPost)).thenReturn(httpResponse);
+        when(httpClient.execute(httpPost)).thenReturn(httpResponse);
+
+        assertEquals(Result.OK, new Dispatcher().sendMessage(message, httpPost, httpClient, this.context));
+
+        this.verifyHttpPost(message, httpPost);
+
+        ArgumentCaptor<ByteArrayEntity> byteArrayEntityCaptor = ArgumentCaptor.forClass(ByteArrayEntity.class);
+        verify(httpPost).setEntity(byteArrayEntityCaptor.capture());
+        ByteArrayEntity entity = byteArrayEntityCaptor.getValue();
+        assertNull(entity.getContentEncoding());
+        assertEquals(EntityUtils.toString(entity, "utf-8"), "body");
+    }
+
+    @Test
+    public void sendMessageWithGzipHeaderShouldCompressContentAndReturnOK() throws URISyntaxException, ClientProtocolException, IOException, KeyManagementException, UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException, CertificateException {
+        Message message = RepositoryTest.getMessageWithGzipHeaders("this any message body more than 10 bytes length");
+        byte[] data = new String("this any message body more than 10 bytes length").getBytes("utf-8");
+
+        StatusLine statusLine = mock(StatusLine.class);
+        when(statusLine.getStatusCode()).thenReturn(200);
+        HttpResponse httpResponse = mock(HttpResponse.class);
+        when(httpResponse.getStatusLine()).thenReturn(statusLine);
+
+        HttpPost httpPost = mock(HttpPost.class);
+        HttpClient httpClient = mock(HttpClient.class);
+        when(httpClient.execute(httpPost)).thenReturn(httpResponse);
+
+        Result actual = new Dispatcher().sendMessage(message, httpPost, httpClient, this.context);
+
+        assertEquals(Result.OK, actual);
+
+        this.verifyHttpPost(message, httpPost);
+
+        ArgumentCaptor<ByteArrayEntity> entityCaptor = ArgumentCaptor.forClass(ByteArrayEntity.class);
+        verify(httpPost).setEntity(entityCaptor.capture());
+        AbstractHttpEntity byteArrayEntity = entityCaptor.getValue();
+        assertEquals(byteArrayEntity.getContentEncoding().getValue(), "gzip");
+
+        byte[] expected = gzip(data);
+        assertArrayEquals(EntityUtils.toByteArray(byteArrayEntity), expected);
+    }
+
 	private void verifyHttpPost(Message message, HttpPost httpPost) {
 		ArgumentCaptor<URI> argument = ArgumentCaptor.forClass(URI.class);		
 		verify(httpPost).setURI(argument.capture());
 		assertEquals(message.getUrl(), argument.getValue().toString());
-		
-		verify(httpPost).setHeader(message.getHeaders()[0].getKey(), message.getHeaders()[0].getValue());
-		verify(httpPost).setHeader(message.getHeaders()[1].getKey(), message.getHeaders()[1].getValue());
+
+   		verify(httpPost).setHeader(message.getHeaders()[0].getKey(), message.getHeaders()[0].getValue());
+        verify(httpPost).setHeader(message.getHeaders()[1].getKey(), message.getHeaders()[1].getValue());
+        verify(httpPost, times(2)).setHeader(anyString(), anyString());
 	}
+
+    private byte[] gzip(byte[] data) throws IOException {
+        ByteArrayOutputStream arr = new ByteArrayOutputStream();
+        OutputStream zipper = new GZIPOutputStream(arr);
+        zipper.write(data);
+        zipper.close();
+        return arr.toByteArray();
+    }
 }
--- reyna/src/com/b2msolutions/reyna/Dispatcher.java
+++ reyna/src/com/b2msolutions/reyna/Dispatcher.java
@@ -5,80 +5,138 @@ import android.net.http.AndroidHttpClient;
 import com.b2msolutions.reyna.http.HttpPost;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.entity.AbstractHttpEntity;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.protocol.HTTP;
 
 import java.net.URI;
+import java.util.ArrayList;
 
 public class Dispatcher {
 
-	private static final String TAG = "Dispatcher";
+    private static final String TAG = "Dispatcher";
 
     public enum Result {
-		OK, PERMANENT_ERROR, TEMPORARY_ERROR
-	}
+        OK, PERMANENT_ERROR, TEMPORARY_ERROR
+    }
 
-	public Result sendMessage(Context context, Message message) {
-		Logger.v(TAG, "sendMessage");
+    public Result sendMessage(Context context, Message message) {
+        Logger.v(TAG, "sendMessage");
 
         AndroidHttpClient httpClient = AndroidHttpClient.newInstance("Reyna", context);
         try {
-		    return this.sendMessage(message, new HttpPost(), httpClient);
+            return this.sendMessage(message, new HttpPost(), httpClient, context);
         } finally {
             httpClient.close();
         }
-	}
+    }
 
-	protected Result sendMessage(Message message, HttpPost httpPost, HttpClient httpClient) {
+    protected Result sendMessage(Message message, HttpPost httpPost, HttpClient httpClient, Context context) {
         Logger.v(TAG, "sendMessage: injected");
-		
-		Result parseHttpPostResult = this.parseHttpPost(message, httpPost, httpClient);
-		if(parseHttpPostResult != Result.OK) return parseHttpPostResult;
-		
-		return this.tryToExecute(httpPost, httpClient);
-	}
-	
-	private Result parseHttpPost(Message message, HttpPost httpPost, HttpClient httpClient) {
+
+        Result parseHttpPostResult = this.parseHttpPost(message, httpPost, httpClient, context);
+        if (parseHttpPostResult != Result.OK) return parseHttpPostResult;
+
+        return this.tryToExecute(httpPost, httpClient);
+    }
+
+    private Result parseHttpPost(Message message, HttpPost httpPost, HttpClient httpClient, Context context) {
         Logger.v(TAG, "parseHttpPost");
-		
-		try {
-			for (Header header : message.getHeaders()) {
-				httpPost.setHeader(header.getKey(), header.getValue());
-			}
 
-			URI uri = message.getURI();
-			httpPost.setURI(uri);
-            httpPost.setEntity(new StringEntity(message.getBody(), HTTP.UTF_8));
-			return Result.OK;
-		} catch (Exception e) {
-			Logger.e(TAG, "parseHttpPost", e);
-			return Result.PERMANENT_ERROR;
-		}
-	}
+        try {
+            URI uri = message.getURI();
+            httpPost.setURI(uri);
 
-	private Result tryToExecute(HttpPost httpPost, HttpClient httpClient) {
+            AbstractHttpEntity entity = Dispatcher.getEntity(message, context);
+            httpPost.setEntity(entity);
+
+            Dispatcher.setHeaders(httpPost, message.getHeaders());
+
+            return Result.OK;
+        } catch (Exception e) {
+            Logger.e(TAG, "parseHttpPost", e);
+            return Result.PERMANENT_ERROR;
+        }
+    }
+
+    private Result tryToExecute(HttpPost httpPost, HttpClient httpClient) {
         Logger.v(TAG, "tryToExecute");
-		
-		try {
-			HttpResponse response = httpClient.execute(httpPost);
-			return Dispatcher.getResult(response.getStatusLine().getStatusCode());
-		} catch (Exception e) {
+
+        try {
+            HttpResponse response = httpClient.execute(httpPost);
+            return Dispatcher.getResult(response.getStatusLine().getStatusCode());
+        } catch (Exception e) {
             Logger.d(TAG, "tryToExecute", e);
-			Logger.i(TAG, "tryToExecute: temporary error");
-			return Result.TEMPORARY_ERROR;
-		}
-	}
+            Logger.i(TAG, "tryToExecute: temporary error");
+            return Result.TEMPORARY_ERROR;
+        }
+    }
 
-	protected static Result getResult(int statusCode) {
+    protected static Result getResult(int statusCode) {
         Logger.v(TAG, "getResult: " + statusCode);
-		
-		if (statusCode >= 200 && statusCode < 300)
-			return Result.OK;
-		if (statusCode >= 300 && statusCode < 500)
-			return Result.PERMANENT_ERROR;
-		if (statusCode >= 500 && statusCode < 600)
-			return Result.TEMPORARY_ERROR;
 
-		return Result.PERMANENT_ERROR;
-	}
+        if (statusCode >= 200 && statusCode < 300)
+            return Result.OK;
+        if (statusCode >= 300 && statusCode < 500)
+            return Result.PERMANENT_ERROR;
+        if (statusCode >= 500 && statusCode < 600)
+            return Result.TEMPORARY_ERROR;
+
+        return Result.PERMANENT_ERROR;
+    }
+
+    private static boolean shouldGzip(Header[] headers) {
+        for(int i=0; i< headers.length; i++) {
+            Header header = headers[i];
+
+            if (header.getKey().equalsIgnoreCase("content-encoding")
+                && header.getValue().equalsIgnoreCase("gzip")) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static Header[] removeGzipEncodingHeader(Header[] headers) {
+        ArrayList<Header> filteredHeaders = new ArrayList<Header>();
+
+        for(int i=0; i< headers.length; i++) {
+            Header header = headers[i];
+
+            if (header.getKey().equalsIgnoreCase("content-encoding")
+                    && header.getValue().equalsIgnoreCase("gzip")) {
+                continue;
+            }
+
+            filteredHeaders.add(header);
+        }
+
+        Header[] returnedHeaders = new Header[filteredHeaders.size()];
+        return filteredHeaders.toArray(returnedHeaders);
+    }
+
+    private static AbstractHttpEntity getCompressedEntity(String content, Context context) throws Exception{
+        byte[] data = content.getBytes();
+        return AndroidHttpClient.getCompressedEntity(data, context.getContentResolver());
+    }
+
+    private static void setHeaders(HttpPost httpPost, Header[] headers) {
+        Header[] filteredHeaders = Dispatcher.removeGzipEncodingHeader(headers);
+
+        for (Header header : filteredHeaders) {
+            httpPost.setHeader(header.getKey(), header.getValue());
+        }
+    }
+
+    private static AbstractHttpEntity getEntity(Message message, Context context) throws Exception {
+        String content = message.getBody();
+        AbstractHttpEntity entity = new StringEntity(content, HTTP.UTF_8);
+
+        if (Dispatcher.shouldGzip(message.getHeaders())) {
+            entity = getCompressedEntity(content, context);
+        }
+
+        return entity;
+    }
 }
