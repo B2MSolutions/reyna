ac9c6bdf0833d88e497d244a6b8079f263f24718
youhana-hana <meyouhana@gmail.com>
2015-09-01 14:48:01 +0100

    remove using artifacts

--- reyna-test/src/com/b2msolutions/reyna/blackout/TimeRangeTest.java
+++ reyna-test/src/com/b2msolutions/reyna/blackout/TimeRangeTest.java
@@ -8,36 +8,36 @@ import static junit.framework.Assert.assertTrue;
 public class TimeRangeTest {
     @Test
     public void whenTimeIsWithinRangeContainsShouldReturnTrue() {
-        com.b2msolutions.reyna.blackout.Time time = new com.b2msolutions.reyna.blackout.Time(12, 00);
-        com.b2msolutions.reyna.blackout.TimeRange range = new com.b2msolutions.reyna.blackout.TimeRange(new com.b2msolutions.reyna.blackout.Time(11, 00), new com.b2msolutions.reyna.blackout.Time(12, 01));
+        Time time = new Time(12, 00);
+        TimeRange range = new TimeRange(new Time(11, 00), new Time(12, 01));
         assertTrue(range.contains(time));
 
-        time = new com.b2msolutions.reyna.blackout.Time(11, 02);
+        time = new Time(11, 02);
         assertTrue(range.contains(time));
 
-        time = new com.b2msolutions.reyna.blackout.Time(04, 00);
-        range = new com.b2msolutions.reyna.blackout.TimeRange(new com.b2msolutions.reyna.blackout.Time(21, 00), new com.b2msolutions.reyna.blackout.Time(05, 00));
+        time = new Time(04, 00);
+        range = new TimeRange(new Time(21, 00), new Time(05, 00));
         assertTrue(range.contains(time));
 
-        range = new com.b2msolutions.reyna.blackout.TimeRange(new com.b2msolutions.reyna.blackout.Time(00, 00), new com.b2msolutions.reyna.blackout.Time(23, 59));
-        time = new com.b2msolutions.reyna.blackout.Time(23, 59);
+        range = new TimeRange(new Time(00, 00), new Time(23, 59));
+        time = new Time(23, 59);
         assertTrue(range.contains(time));
-        time = new com.b2msolutions.reyna.blackout.Time(00, 00);
+        time = new Time(00, 00);
         assertTrue(range.contains(time));
-        time = new com.b2msolutions.reyna.blackout.Time(00, 01);
+        time = new Time(00, 01);
         assertTrue(range.contains(time));
-        time = new com.b2msolutions.reyna.blackout.Time(12, 00);
+        time = new Time(12, 00);
         assertTrue(range.contains(time));
     }
 
     @Test
     public void whenTimeIsNotWithinRangeContainsShouldReturnFalse() {
-        com.b2msolutions.reyna.blackout.Time time = new com.b2msolutions.reyna.blackout.Time(12, 02);
-        com.b2msolutions.reyna.blackout.TimeRange range = new com.b2msolutions.reyna.blackout.TimeRange(new com.b2msolutions.reyna.blackout.Time(11, 00), new com.b2msolutions.reyna.blackout.Time(12, 01));
+        Time time = new Time(12, 02);
+        TimeRange range = new TimeRange(new Time(11, 00), new Time(12, 01));
         assertFalse(range.contains(time));
 
-        time = new com.b2msolutions.reyna.blackout.Time(19, 58);
-        range = new TimeRange(new com.b2msolutions.reyna.blackout.Time(19, 59), new com.b2msolutions.reyna.blackout.Time(19, 57));
+        time = new Time(19, 58);
+        range = new TimeRange(new Time(19, 59), new Time(19, 57));
         assertFalse(range.contains(time));
     }
 }
--- reyna-test/src/com/b2msolutions/reyna/blackout/TimeTest.java
+++ reyna-test/src/com/b2msolutions/reyna/blackout/TimeTest.java
@@ -9,53 +9,53 @@ import static junit.framework.Assert.*;
 public class TimeTest {
     @Test
     public void isBeforeShouldReturnExpected() {
-        assertTrue(new com.b2msolutions.reyna.blackout.Time(12, 00).isBeforeOrEqualTo(new com.b2msolutions.reyna.blackout.Time(12, 01)));
-        assertTrue(new com.b2msolutions.reyna.blackout.Time(11, 00).isBeforeOrEqualTo(new com.b2msolutions.reyna.blackout.Time(12, 00)));
-        assertFalse(new com.b2msolutions.reyna.blackout.Time(12, 01).isBeforeOrEqualTo(new com.b2msolutions.reyna.blackout.Time(12, 00)));
-        assertTrue(new com.b2msolutions.reyna.blackout.Time(12, 01).isBeforeOrEqualTo(new com.b2msolutions.reyna.blackout.Time(12, 01)));
+        assertTrue(new Time(12, 00).isBeforeOrEqualTo(new Time(12, 01)));
+        assertTrue(new Time(11, 00).isBeforeOrEqualTo(new Time(12, 00)));
+        assertFalse(new Time(12, 01).isBeforeOrEqualTo(new Time(12, 00)));
+        assertTrue(new Time(12, 01).isBeforeOrEqualTo(new Time(12, 01)));
     }
 
     @Test
     public void isAfterShouldReturnExpected() {
-        assertFalse(new com.b2msolutions.reyna.blackout.Time(12, 00).isAfterOrEqualTo(new com.b2msolutions.reyna.blackout.Time(12, 01)));
-        assertFalse(new com.b2msolutions.reyna.blackout.Time(11, 00).isAfterOrEqualTo(new com.b2msolutions.reyna.blackout.Time(12, 00)));
-        assertTrue(new com.b2msolutions.reyna.blackout.Time(12, 01).isAfterOrEqualTo(new com.b2msolutions.reyna.blackout.Time(12, 00)));
-        assertTrue(new com.b2msolutions.reyna.blackout.Time(12, 01).isAfterOrEqualTo(new com.b2msolutions.reyna.blackout.Time(12, 01)));
+        assertFalse(new Time(12, 00).isAfterOrEqualTo(new Time(12, 01)));
+        assertFalse(new Time(11, 00).isAfterOrEqualTo(new Time(12, 00)));
+        assertTrue(new Time(12, 01).isAfterOrEqualTo(new Time(12, 00)));
+        assertTrue(new Time(12, 01).isAfterOrEqualTo(new Time(12, 01)));
     }
 
     @Test
     public void shouldSetMinuteOfDay() {
-        assertEquals(12 * 60 + 1, new com.b2msolutions.reyna.blackout.Time(12, 01).getMinuteOfDay());
-        assertEquals(1000, new com.b2msolutions.reyna.blackout.Time(1000).getMinuteOfDay());
+        assertEquals(12 * 60 + 1, new Time(12, 01).getMinuteOfDay());
+        assertEquals(1000, new Time(1000).getMinuteOfDay());
     }
 
     @Test(expected = InvalidParameterException.class)
     public void shouldThrowOnConstructionIfHourTooLarge() {
-        new com.b2msolutions.reyna.blackout.Time(24, 00);
+        new Time(24, 00);
     }
 
     @Test(expected = InvalidParameterException.class)
     public void shouldThrowOnConstructionIfMinutesTooLarge() {
-        new com.b2msolutions.reyna.blackout.Time(23, 60);
+        new Time(23, 60);
     }
 
     @Test(expected = InvalidParameterException.class)
     public void shouldThrowOnConstructionIfHourTooSmall() {
-        new com.b2msolutions.reyna.blackout.Time(-1, 00);
+        new Time(-1, 00);
     }
 
     @Test(expected = InvalidParameterException.class)
     public void shouldThrowOnConstructionIfMinutesTooSmall() {
-        new com.b2msolutions.reyna.blackout.Time(23, -1);
+        new Time(23, -1);
     }
 
     @Test(expected = InvalidParameterException.class)
     public void shouldThrowOnConstructionIfMinuteOfDayTooSmall() {
-        new com.b2msolutions.reyna.blackout.Time(-1);
+        new Time(-1);
     }
 
     @Test(expected = InvalidParameterException.class)
     public void shouldThrowOnConstructionIfMinuteOfDayTooLarge() {
-        new com.b2msolutions.reyna.blackout.Time(24 * 60);
+        new Time(24 * 60);
     }
 }
